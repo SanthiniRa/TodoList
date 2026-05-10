f614a0047523c5bb614bf05c3488b2c2a6454aa1
SanthiniRa <santhinirajan@icloud.com>
2026-05-10 20:54:55 +0100

    remove hardcoded value

--- src/supabaseClient.tsx
+++ src/supabaseClient.tsx
@@ -1,15 +1,17 @@
-import { createClient } from '@supabase/supabase-js'
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(
-  'https://hvgsjchmchkgbnfkuhur.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh2Z3NqY2htY2hrZ2JuZmt1aHVyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc3MzQ0NTMsImV4cCI6MjA5MzMxMDQ1M30.ShhX8C2suVvGBV6qjZylIBpuXhN8iGLyOm0m5e-gIWU'
-  ,
-    {
-      auth: {
-        persistSession: true,
-        autoRefreshToken: true,
-        detectSessionInUrl: true,
-        flowType: 'pkce',
-      },
-    }
-  )
+  supabaseUrl,
+  supabaseAnonKey,
+  {
+    auth: {
+      persistSession: true,
+      autoRefreshToken: true,
+      detectSessionInUrl: true,
+      flowType: 'pkce',
+    },
+  }
+);
